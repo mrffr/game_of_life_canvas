49bc42659cc84c707d38d558ebfe1b9948ea4d3b
F.Fitzgerald <mrffitzgerald@runbox.com>
2018-08-01 01:10:18 +0100

    added draw func

--- game_of_life.js
+++ game_of_life.js
@@ -7,11 +7,26 @@ function drawCanvas(myBoard, canvas, context) {
   context.clearRect(0, 0, canvas.width, canvas.height);
 }
 
+var myBoard = {
+    cols : 20,
+    rows : 20,
+    board : new Array(400)
+};
+
+function fillBoard(myBoard){
+}
+fillBoard(myBoard);
+
+var interv;
 function clickButton() {
     var button = document.getElementById("startButton");
   if(button.innerText == "Start"){
       button.innerText = "Stop";
+      fillBoard(myBoard);
+      drawCanvas(myBoard, canvas, context);
+      interv = setInterval(drawCanvas, 1000, myBoard, canvas, context);
   }else{
-    button.innerText = "Start";
+      button.innerText = "Start";
+      clearInterval(interv);
   }
 };
